510b2b6fc3d6b74862f41a10df15f2121a72fb0d
unknown <valy_empire@yahoo.com>
2023-06-30 20:41:20 +0300

    Updated the skills variable and added more images in the assets to be used in the HomeSkills component

--- src/utils/skills.ts
+++ src/utils/skills.ts
@@ -1,29 +1,56 @@
 import { v4 as uuid } from "uuid";
+import html_img from "../assets/html.png";
+import css_img from "../assets/css.png";
+import javascript_img from "../assets/javascript.png";
+import react_img from "../assets/react.png";
+import bootstrap_img from "../assets/bootstrap.png";
+import typescript_img from "../assets/typescript1.png";
+import material_img from "../assets/material_ui.png";
+import node_img from "../assets/node.png";
+import express_img from "../assets/express.png";
+import postgres_img from "../assets/Postgresql_elephant.svg.png";
+import vscode_img from "../assets/vscode.png";
+import github_img from "../assets/github.png";
+import git_img from "../assets/git2.png";
 
 export const skills = [
   {
     id: uuid(),
-    category: "Front-end",
-    skills: [
-      "HTML5",
-      "CSS3",
-      "JavaScript",
-      "TypeScript",
-      "React",
-      "Bootstrap4",
-      "Material-UI",
-      "NextJS",
+    title: "Front End",
+    cardText:
+      "Enhancing web performance and responsiveness while addressing cross-browser compatibility challenges and ensuring accessibility are the primary focus of my work.",
+    images: [
+      { src: html_img, alt: "html" },
+      { src: css_img, alt: "css" },
+      { src: javascript_img, alt: "javascript" },
+      { src: react_img, alt: "Ireact" },
+      { src: bootstrap_img, alt: "bootstrap" },
+      { src: typescript_img, alt: "typescript" },
+      { src: material_img, alt: "material" },
     ],
   },
   {
     id: uuid(),
-    category: "Back-end",
-    skills: ["NodeJS", "TypeScript", "ExpressJS", "MongoDB", "PostgreSQL"],
+    title: "Back End",
+    cardText:
+      "I thrive on ensuring the security, scalability, and efficient handling of application logic in the back end. It's one of the aspects I find most enjoyable in my work.",
+    images: [
+      { src: node_img, alt: "node" },
+      { src: express_img, alt: "express" },
+      { src: postgres_img, alt: "postgres" },
+      { src: typescript_img, alt: "typescript" },
+    ],
   },
   {
     id: uuid(),
-    category: "Tools",
-    skills: ["VS Code", "Git", " GitHub", "Postman", "Netlify"],
+    title: "Tools",
+    cardText:
+      "To create exceptional applications, I rely on outstanding development tools and dependable services. They form the foundation for building remarkable digital experiences.",
+    images: [
+      { src: vscode_img, alt: "vscode" },
+      { src: github_img, alt: "github" },
+      { src: git_img, alt: "git" },
+    ],
   },
 ];
 
